0a450fac7e80417b02fb48a37e109e7ef4070377
ColePriser <cpriser@purdue.edu>
2022-09-03 16:40:38 -0400

    Retrieve grid, check if grid is full, and play functionality complete

--- Game.py
+++ Game.py
@@ -7,7 +7,7 @@ class Game:
         self.grid = [[0, 0, 0] for _ in range(3)]
 
         # User is specified by 1, Agent is specified by -1
-        self.player = 1
+        self.cur_player = 1
 
         # Associate tic-tac-toe symbols with numbers for simplicity
         self.symbols = {1: "X", -1: "O", 0: "-"}
@@ -18,31 +18,76 @@ class Game:
 
     def get_winner(self):
         # Checks if either player has won horizontally
-        for x in range(3):
-            if sum(self.grid[x]) == 3:
+        for row in range(3):
+            if sum(self.grid[row]) == 3:
                 return 1
                 # If user has three in a row horizontally, return 1
-            elif sum(self.grid[x]) == -3:
+            elif sum(self.grid[row]) == -3:
                 return -1
                 # If agent has three in a row horizontally, return -1
 
         # Checks if either player has won vertically
-        for x in range(3):
-            if sum(self.grid[x][y] for y in range(3)) == 3:
+        for row in range(3):
+            if sum(self.grid[row][col] for col in range(3)) == 3:
                 return 1
                 # If user has three in a row vertically, return 1
-            elif sum(self.grid[x][y] for y in range(3)) == -3:
+            elif sum(self.grid[row][col] for col in range(3)) == -3:
                 return -1
                 # If agent has three in a row vertically, return -1
 
         # Checks if either player has won diagonally
-        if sum(self.grid[x][x] for x in range(3)) == 3:
+        if sum(self.grid[row][row] for row in range(3)) == 3:
             return 1
             # If user has three in a row diagonally, return 1
-        elif sum(self.grid[x][x] for x in range(3)) == -3:
+        elif sum(self.grid[row][row] for row in range(3)) == -3:
             return -1
             # If agent has three in a row diagonally, return -1
 
         return 0
         # If no player has won, then return 0
+
+    def get_available_points(self):
+        # Return array of locations that don't have X or O
+        available = []
+        for row in range(3):
+            for col in range(3):
+                if self.grid[row][col] == 0:
+                    available.append[row][col]
+        return available
+
+    def full_grid(self):
+        # If every location has an X or O, then return True
+        for row in range(3):
+            for col in range(3):
+                if self.grid[row][col] == 0:
+                    return False
+        return True
+
+    def _print_grid(self):
+        # Print the current grid
+        for row in self.grid:
+            for cur in row:
+                print(self.symbols[cur], end="\t")
+            print("\n")
+        print("________________\n")
+
+    def play(self, row, col):
+        if self.grid[row][col] != 0:
+            return None
+            # If the desired location is taken, return None
+        # Otherwise, assign desired location to current player number
+        self.grid[row][col] = self.cur_player
+        if self.display:
+            self._print_grid()
+            # If display is true, then print the current grid
+
+        if self.get_winner() == 1:
+            return 1
+            # If user has won, then return 1
+        elif self.get_winner() == -1:
+            return -1
+            # If agent has won, then return -1
+        # Change current player
+        self.cur_player *= -1
+        return None
 
